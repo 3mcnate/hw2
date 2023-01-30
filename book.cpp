64867ed88f072bbe990647d80cecd3cf3b88a6cd
Nate Boxer <nboxer@usc.edu>
2023-01-29 23:07:58 -0800

    Tasks 1 and 2 done but not tested

---
+++ book.cpp
@@ -0,0 +1,11 @@
+#include "book.h"
+#include "util.h"
+
+using namespace std;
+
+std::set<std::string> Book::keywords() const
+{
+    std::set<std::string> allKeywords;
+    
+
+}
--- util.h
+++ util.h
@@ -13,20 +13,28 @@
 template <typename T>
 std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
 {
+    std::set<T> intersection;
+   
+    typename set<T>::iterator it;
+    for (it = s1.begin(); it != s1.end(); ++it) {
+        if (s2.find(*it) != s2.end()) {
+            intersection.insert(*it);
+        }
+    }
 
-
-
-
-
+    return intersection;
 }
 template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
+    std::set<T> setunion = s1;
 
+    typename set<T>::iterator it;
+    for (it = s2.begin(); it != s2.end(); ++it) {
+        setunion.insert(*it);
+    }
 
-
-
-
+    return setunion;
 }
 
 /***********************************************/
